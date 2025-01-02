f8e0450239659da76e97959227c09abbb8e952f6
ErBW_s <erbws@foxmail.com>
2025-01-03 01:05:07 +0800

    add example app

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:card_settings_ui/card_settings_ui.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,119 +8,210 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: Colors.blue, brightness: Brightness.light),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      darkTheme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: Colors.blue, brightness: Brightness.dark),
+      ),
+      home: const SettingsPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class SettingsPage extends StatefulWidget {
+  const SettingsPage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<SettingsPage> createState() => _SettingsPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class _SettingsPageState extends State<SettingsPage> {
+  bool isEnabled = true;
+  bool useFingerprint = true;
+  double size = 10.0;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Settings'),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        child: SizedBox(
+          width: (MediaQuery.of(context).size.width > 800) ? 800 : null,
+          child: SettingsList(
+            sections: [
+              SettingsSection(
+                title: Text('Common'),
+                tiles: <SettingsTile>[
+                  SettingsTile.navigation(
+                    onPressed: (_) =>
+                        showToast(context, 'Navigate to history page'),
+                    leading: Icon(Icons.history),
+                    title: Text('History'),
+                  ),
+                  SettingsTile.navigation(
+                    onPressed: (_) =>
+                        showToast(context, 'Navigate to extension page'),
+                    leading: Icon(Icons.extension),
+                    title: Text('Extension'),
+                  ),
+                ],
+              ),
+              SettingsSection(
+                title: Text('Security'),
+                bottomInfo: Text('Allow user log in with fingerprint'),
+                tiles: <SettingsTile>[
+                  SettingsTile.navigation(
+                    onPressed: (_) =>
+                        showToast(context, 'Navigate to email editing page'),
+                    leading: Icon(Icons.email_rounded),
+                    title: Text('Email'),
+                  ),
+                  SettingsTile.switchTile(
+                    onToggle: (value) {
+                      useFingerprint = value ?? !useFingerprint;
+                      setState(() {});
+                    },
+                    initialValue: useFingerprint,
+                    leading: Icon(Icons.fingerprint),
+                    title: Text('Enable fingerprint'),
+                  ),
+                ],
+              ),
+              SettingsSection(
+                title: Text('Subtitle'),
+                tiles: <SettingsTile>[
+                  SettingsTile.switchTile(
+                    onToggle: (value) {
+                      isEnabled = value ?? !isEnabled;
+                      setState(() {});
+                    },
+                    initialValue: isEnabled,
+                    leading: Icon(Icons.subtitles),
+                    title: Text('Enable subtitle'),
+                  ),
+                  SettingsTile.navigation(
+                    onPressed: (_) => subtitleSizeDialog(context),
+                    enabled: isEnabled,
+                    leading: Icon(Icons.format_size),
+                    title: Text('Subtitle size'),
+                    value: Text('$size'),
+                  ),
+                ],
+              ),
+              SettingsSection(
+                bottomInfo: Text('Upload settings to cloud'),
+                tiles: <SettingsTile>[
+                  SettingsTile(
+                    onPressed: (_) => showToast(context, 'Upload successfully'),
+                    title: Text('Upload'),
+                    trailing: Icon(Icons.cloud_upload_rounded),
+                  ),
+                ],
+              ),
+              SettingsSection(
+                bottomInfo: Text('Download settings from cloud'),
+                tiles: <SettingsTile>[
+                  SettingsTile(
+                    onPressed: (_) {
+                      showToast(context, 'Download successfully');
+                    },
+                    title: Text('Download'),
+                    trailing: Icon(Icons.cloud_download_rounded),
+                  ),
+                ],
+              ),
+              SettingsSection(
+                tiles: <SettingsTile>[
+                  SettingsTile.navigation(
+                    onPressed: (_) =>
+                        showToast(context, 'Log out successfully'),
+                    leading: Icon(Icons.logout),
+                    title: Text('Log out'),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
+  Future subtitleSizeDialog(BuildContext context) {
+    return showDialog(
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Subtitle font size'),
+          content: StatefulBuilder(builder: (BuildContext context, _) {
+            return Wrap(
+              spacing: 8,
+              runSpacing: 8,
+              children: [
+                for (final double i in subtitleSize) ...<Widget>[
+                  if (i == size) ...<Widget>[
+                    FilledButton(
+                      onPressed: () async {
+                        setState(() {
+                          size = i;
+                        });
+                        Navigator.of(context).pop();
+                      },
+                      child: Text(i.toString()),
+                    ),
+                  ] else ...[
+                    FilledButton.tonal(
+                      onPressed: () async {
+                        setState(() {
+                          size = i;
+                        });
+                        Navigator.of(context).pop();
+                      },
+                      child: Text(i.toString()),
+                    ),
+                  ]
+                ]
+              ],
+            );
+          }),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: Text(
+                'Cancel',
+                style: TextStyle(color: Theme.of(context).colorScheme.outline),
+              ),
+            ),
+          ],
+        );
+      },
+      context: context,
+    );
+  }
+
+  void showToast(BuildContext context, String message) {
+    ScaffoldMessenger.of(context)
+      ..removeCurrentSnackBar()
+      ..showSnackBar(
+        SnackBar(
+          content: Text(message),
+        ),
+      );
+  }
 }
+
+final List<double> subtitleSize = [
+  10.0,
+  11.0,
+  12.0,
+  13.0,
+  14.0,
+  15.0,
+];
