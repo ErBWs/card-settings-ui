2fa25cb5c35acd888a05199510794d9befa23501
ErBW_s <erbws@foxmail.com>
2025-05-17 00:27:15 +0800

    feat: easier way to configure max width

--- lib/list/settings_list.dart
+++ lib/list/settings_list.dart
@@ -5,34 +5,34 @@ class SettingsList extends StatelessWidget {
   const SettingsList({
     required this.sections,
     this.shrinkWrap = false,
+    this.maxWidth,
     this.physics,
     this.contentPadding,
     super.key,
   });
 
   final bool shrinkWrap;
+  final double? maxWidth;
   final ScrollPhysics? physics;
   final EdgeInsetsGeometry? contentPadding;
   final List<AbstractSettingsSection> sections;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      alignment: Alignment.center,
-      child: LayoutBuilder(
-        builder: (context, layout) {
-          return ListView.builder(
-            physics: physics,
-            shrinkWrap: shrinkWrap,
-            itemCount: sections.length,
-            padding: contentPadding ?? const EdgeInsets.symmetric(vertical: 20),
-            itemBuilder: (BuildContext context, int index) {
-              return sections[index];
-            },
-          );
-        },
-      ),
+    return ListView.builder(
+      physics: physics,
+      shrinkWrap: true,
+      itemCount: sections.length,
+      padding: contentPadding ?? const EdgeInsets.symmetric(vertical: 20),
+      itemBuilder: (BuildContext context, int index) {
+        return Align(
+          alignment: Alignment.center,
+          child: Container(
+            constraints: BoxConstraints(maxWidth: maxWidth ?? double.infinity),
+            child: sections[index],
+          ),
+        );
+      },
     );
   }
 }
